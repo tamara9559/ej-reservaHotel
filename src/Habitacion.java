158cceb5b51d472edd77de31eef90c614f3833d9
juan támara <t4mara95@gmail.com>
2025-06-03 20:03:24 -0500

    clases

---
+++ src/Habitacion.java
@@ -0,0 +1,23 @@
+public class Habitacion {
+    private int numero;
+    private boolean disponible;
+
+    public Habitacion(int numero) {
+        this.numero = numero;
+        this.disponible = true;
+    }
+
+    public int getNumero() { return numero; }
+
+    public boolean estaDisponible() { return disponible; }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    @Override
+    public String toString() {
+        return "Habitación " + numero + (disponible ? " (Disponible)" : " (Ocupada)");
+    }
+}
+
